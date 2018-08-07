3ea27655cff8cc08d3406fad245dc7adc92b1a15
ssantic <srdjan.santic@gmail.com>
2018-08-07 17:32:43 +0200

    Fix bug with using imported fucntions

--- Week2/test_num_buses.py
+++ Week2/test_num_buses.py
@@ -8,28 +8,28 @@ class TestNumBuses(unittest.TestCase):
     # Add your test methods for a1.num_buses here.
     def test_one_passenger(self):
     """Test with only one passenger present."""
-    actual = num_buses(1)
+    actual = a1.num_buses(1)
     expected = 1
 
     self.assertEqual(actual, expected)
 
     def test_exact_multiple(self):
     """Test with exactly 50 passengers."""
-    actual = num_buses(50)
+    actual = a1.num_buses(50)
     expected = 1
 
     self.assertEqual(actual, expected)
 
     def test_not_exact_multiple(self):
     """Test with a value that isn't divisible by 50."""
-    actual = num_buses(125)
+    actual = a1.num_buses(125)
     expected = 3
 
     self.assertEqual(actual, expected)
 
     def test_several_multiples(self):
     """Test with a value that is several times over 50."""
-    actual = num_buses(300)
+    actual = a1.num_buses(300)
     expected = 6
 
     self.assertEqual(actual, expected)

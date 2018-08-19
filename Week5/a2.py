809914a9f2eeb9b0dba4c6ed67870633aff1a090
ssantic <srdjan.santic@gmail.com>
2018-08-19 11:52:32 +0200

    Implement Rat methods

--- Week5/a2.py
+++ Week5/a2.py
@@ -37,6 +37,64 @@ class Rat:
     """ A rat caught in a maze. """
 
     # Write your Rat methods here.
+    def __init__(self, symbol, row, col):
+        """(Rat, str, int, int) -> NoneType
+
+        Initialise a Rat
+
+        >>> rat = Rat('P', 2, 3)
+        >>> rat.symbol
+        'P'
+        >>> rat.row
+        2
+        >>> rat.col
+        3
+        >>> rat.num_sprouts_eaten
+        0
+        """
+        self.symbol = symbol
+        self.row = row
+        self.col = col
+        self.num_sprouts_eaten = 0
+
+    def set_location(self, row, col):
+        """(Rat, int, int) -> NoneType
+
+        Set the location for the Rat in the Maze
+
+        >>> rat = Rat('P', 2, 3)
+        >>> rat.set_location(4, 5)
+        >>> rat.row
+        4
+        """
+        self.row = row
+        self.col = col
+
+    def eat_sprout(self):
+        """(Rat) -> NoneType
+
+        Eat a sprout in the Maze
+
+        >>> rat = Rat('P', 2, 3)
+        >>> rat.eat_sprout()
+        >>> rat.num_sprouts_eaten
+        1
+        """
+        self.num_sprouts_eaten += 1
+
+    def __str__(self):
+        """
+        (Rat) -> str
+
+        Print information about the Rat
+
+        >>> rat = Rat('P', 2, 3)
+        >>> print(rat)
+        'J at (2, 3) ate 2 sprouts.'
+        """
+        return "{} at ({}, {}) ate {} sprouts.".format(
+            self.symbol, self.row, self.col, self.num_sprouts_eaten)
+
 
 class Maze:
     """ A 2D maze. """
